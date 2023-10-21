ba7bcaa261b8a57c28b017503cc34343e683a664
Ruben <adityaprasad7123@gmail.com>
2023-10-21 14:10:04 -0400

    Made form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Ruben",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Cris",
+    added: new Date()
+  }
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Message Board', messages: messages });
 });
+
+router.get('/new', (req, res) => {
+  res.render('form', { title: 'Sign Up Form' })
+})
+
+router.post('/new', (req, res) => {
+  const messageUser = req.body.messageUser
+  const messageText = req.body.messageText
+
+  messages.push({ text: messageText, user: messageUser, added: new Date() })
+
+  res.redirect("/")
+})
 
 module.exports = router;
